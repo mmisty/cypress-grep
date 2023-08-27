51b3301e5d2b54fcc51810bb672f36d824a90482
mmisty <minydas@yandex.ru>
2023-08-27 17:54:02 +0300

    fix cov 1

--- tests/test-folder/integration/run-script.test.ts
+++ tests/test-folder/integration/run-script.test.ts
@@ -16,7 +16,7 @@ const deleteResults = () => {
 
 const runTests = (specPattern: string, args: string[] = []) => {
   execSync(
-    `cd ${process.cwd()} && node ./.bin/cy-grep.js --script 'CYPRESS_COVERAGE=true npm run cy:run -- --config ${specPattern}' ${args.join(
+    `cd ${process.cwd()} && node ./.bin/cy-grep.js --script 'COVERAGE_REPORT_DIR=reports/coverage-cypress CYPRESS_COVERAGE=true npm run cy:run -- --config ${specPattern}' ${args.join(
       ' ',
     )}`,
     {
