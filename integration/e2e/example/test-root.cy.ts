701446bc80f59627c39134807ac0a5a4bbec6abf
Taisia Pitko <taisia.pitko@greenlight.guru>
2025-01-10 01:12:59 +0100

    fix tests

--- integration/e2e/example/test-root.cy.ts
+++ integration/e2e/example/test-root.cy.ts
@@ -8,8 +8,8 @@ it(`test ${tag('P1', 'critical')}`, { tags: [tag('issue', 'example of info')] },
   const infoForIssue = this.test?.tags?.find(t => t.tag === '@issue');
   const infoForP1 = this.test?.tags?.find(t => t.tag === '@P1');
 
-  expect(infoForIssue).to.deep.eq({ tag: '@issue', info: ['example of info'] });
-  expect(infoForP1).to.deep.eq({ tag: '@P1', info: ['critical'] });
+  expect(infoForIssue).to.deep.eq({ tag: '@issue', info: ['example of info'], isOwnTag: true });
+  expect(infoForP1).to.deep.eq({ tag: '@P1', info: ['critical'], isOwnTag: true });
   cy.log(`infoForIssue: **${infoForIssue?.info?.join('')}**`);
   cy.log(`infoForP1: **${infoForP1?.info?.join('')}**`);
 });
--- integration/e2e/regression/several-similar.test.ts
+++ integration/e2e/regression/several-similar.test.ts
@@ -2,16 +2,16 @@ describe('suite', () => {
   it('test @issue("123") @issue("456")', function () {
     expect(this.test?.tags).to.have.length(2);
     expect(this.test?.tags).deep.eq([
-      { tag: '@issue', info: ['123'] },
-      { tag: '@issue', info: ['456'] },
+      { tag: '@issue', info: ['123'], isOwnTag: true },
+      { tag: '@issue', info: ['456'], isOwnTag: true },
     ]);
   });
 
   it('test with several args @issue("123","description") @issue("456")', function () {
     expect(this.test?.tags).to.have.length(2);
     expect(this.test?.tags).deep.eq([
-      { tag: '@issue', info: ['123', 'description'] },
-      { tag: '@issue', info: ['456'] },
+      { tag: '@issue', info: ['123', 'description'], isOwnTag: true },
+      { tag: '@issue', info: ['456'], isOwnTag: true },
     ]);
   });
 });
